e7774ca0500dc200bc249c79cc322a5eac3908ed
hello-saif <mdsaifuddinnobab5@gmail.com>
2024-05-02 18:10:31 +0600

    update Score

--- lib/FlashCardScreen.dart
+++ lib/FlashCardScreen.dart
@@ -12,14 +12,24 @@ class FlashcardScreen extends StatefulWidget {
 class _FlashcardScreenState extends State<FlashcardScreen> {
   List<Flashcard> flashcards = [
     Flashcard(question: 'What is the capital of France?', answer: 'Paris'),
-    Flashcard(question: 'What is the largest planet in our solar system?', answer: 'Jupiter'),
-    Flashcard(question: 'Who wrote "Romeo and Juliet"?', answer: 'William Shakespeare'),
-    Flashcard(question: 'What is the powerhouse of the cell?', answer: 'Mitochondria'),
-    Flashcard(question: 'Who painted the Mona Lisa?', answer: 'Leonardo da Vinci'),
-    Flashcard(question: 'What is the chemical symbol for water?', answer: 'H2O'),
-    Flashcard(question: 'Who invented the light bulb?', answer: 'Thomas Edison'),
+    Flashcard(
+        question: 'What is the largest planet in our solar system?',
+        answer: 'Jupiter'),
+    Flashcard(
+        question: 'Who wrote "Romeo and Juliet"?',
+        answer: 'William Shakespeare'),
+    Flashcard(
+        question: 'What is the powerhouse of the cell?',
+        answer: 'Mitochondria'),
+    Flashcard(
+        question: 'Who painted the Mona Lisa?', answer: 'Leonardo da Vinci'),
+    Flashcard(
+        question: 'What is the chemical symbol for water?', answer: 'H2O'),
+    Flashcard(
+        question: 'Who invented the light bulb?', answer: 'Thomas Edison'),
     Flashcard(question: 'What is the tallest mammal?', answer: 'Giraffe'),
-    Flashcard(question: 'Which planet is known as the Red Planet?', answer: 'Mars'),
+    Flashcard(
+        question: 'Which planet is known as the Red Planet?', answer: 'Mars'),
     Flashcard(question: 'What is the chemical symbol for gold?', answer: 'Au'),
   ];
 
@@ -35,8 +45,10 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-
-          Text('Score: $correctAnswers / $totalQuestions'),
+          Text(
+            'Score: $correctAnswers / $totalQuestions',
+            style: const TextStyle(fontSize: 20),
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: flashcards.length,
